e4975a12312c91acf8cfc82271af79ad8890c845
Alfredo E. <alfredoescalante89@gmail.com>
2015-03-10 12:42:48 -0430

    prueba de cambio de archivo y commit

--- app/models/daw_actividad.rb
+++ app/models/daw_actividad.rb
@@ -6,3 +6,6 @@ class DawActividad < ActiveRecord::Base
 	validates :act_nombre, :act_descripcion, :act_disponible, :act_duracion, :act_estado, :act_cantidadpreg, presence: true
 	validates :act_descripcion, length: { minimum: 2, maximum: 1500 }
 end
+
+def pruebaGitHub
+	end
